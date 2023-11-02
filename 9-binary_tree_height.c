d5a1e978bab169a8b2c5feea953041de6c3b7b58
DAVENJAGI <njagidave39@gmail.com>
2023-11-02 07:47:13 -0400

    binary_trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t right_height, left_height;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	else
+	left_height =  binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		return 1 + binary_tree_height(tree->left) + binary_tree_height(tree->right);
+		return (0);
 	}
+	return (1 + (right_height > left_height ? left_height : right_height));
 }
